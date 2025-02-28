ce69abec08359a6d8bac517b5d9e01b6d59afe88
Henrik Norlin <henrik.norl@gmail.com>
2025-02-28 05:19:12 +0100

    account_financial_report, account_reconcile_oca, account_tax_balance

--- addons/dependencies/__manifest__.py
+++ addons/dependencies/__manifest__.py
@@ -3,6 +3,9 @@
     "name": "Dependencies",
     "author": "Loym",
     "depends": [
+        "account_financial_report",
+        "account_reconcile_oca",
+        "account_tax_balance",
         "auth_admin_passkey",
     ],
     "version": "18.0.1.0.0",

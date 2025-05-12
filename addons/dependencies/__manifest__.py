dcb00e8e94f593c9b39d7e47c5083430e5499dd6
Henrik Norlin <henrik@loym.com>
2025-05-12 18:31:10 +0000

    dbfilter_from_header

--- addons/dependencies/__manifest__.py
+++ addons/dependencies/__manifest__.py
@@ -7,6 +7,7 @@
         "account_reconcile_oca",
         "account_tax_balance",
         "auth_admin_passkey",
+        "dbfilter_from_header",
         "document_page",
     ],
     "version": "18.0.1.0.1",
